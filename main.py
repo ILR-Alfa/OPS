e083252e4c201cc4934b6e31c4f5471f04e16bc4
ILR <verbar865@gmail.com>
2025-05-02 20:24:15 +0500

    заготовка

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# заготовка под проект
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+
+
+API_TOKEN = ''
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я эхо-бот. Напиши мне что-нибудь!")
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def echo_message(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    print("Бот запущен...")
+    executor.start_polling(dp, skip_updates=True)
